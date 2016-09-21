c6e4eac60e592081038c386423e5b1cb3b86b230
YANG Jing <jyang@thoughtworks.com>
2016-09-21 13:16:47 +0800

    [JY] - create new session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+	def index
+		@sessions = Session.all
+	end
+
+	def show
+		@session = Session.find(params[:id])
+	end
+
+	def new
+		@session = Session.new
+	end
+
+	def create
+		@session = Session.new(session_params)
+		if @session.save
+			redirect_to @session
+		else
+			render 'new'
+		end
+	end
+	
+	private
+		def session_params
+			params.require(:session).permit(:title, :text)
+		end
+end
---
+++ app/models/session.rb
@@ -0,0 +1,3 @@
+class Session < ActiveRecord::Base
+	validates :title, presence: true, length: { minimum: 5}
+end
